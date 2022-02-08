5526cd62056433c87353bbd105f3d29168f3c840
kaixa <690750353@qq.com>
2022-02-08 22:12:09 +0800

    fix(stroe): display error and exit on error

--- src/main/config/store.ts
+++ src/main/config/store.ts
@@ -2,7 +2,7 @@ import { readFile } from 'fs/promises';
 import { ConfigConst } from '@main/config/const';
 import { join } from 'path';
 import { Keys } from '@common/config/keys';
-import type { BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import { ClientConfigurationType } from '@common/config/type';
 
 const configPath = join(ConfigConst.ConfigDir, Keys.clientConfiguration);
@@ -21,8 +21,23 @@ export class ConfigStore {
   }
 
   async readConfig() {
-    const buffer = await readFile(configPath);
-    this.#config = JSON.parse(buffer.toString());
+    try {
+      const buffer = await readFile(configPath);
+      this.#config = JSON.parse(buffer.toString());
+    } catch (err) {
+      if (err instanceof Error) {
+        dialog.showErrorBox(err.name, err.message);
+      } else {
+        const toString = (err as any).toString ?? Object.prototype.toString;
+        dialog.showErrorBox(
+          `A error occurred when loading configuration file`,
+          `error: ${toString.call(err)}\nThis error is occurred when reading ${configPath}`
+        );
+      }
+      if (this.#config === null) {
+        app.quit();
+      }
+    }
   }
 
   get config() {
