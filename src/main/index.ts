c45d2cc52c86d38fc74a07dae0f85e30ec11a6c9
kaixa <690750353@qq.com>
2022-02-08 23:00:23 +0800

    refactor(MainWindow): use MainWindow wrap logics

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,46 +1,16 @@
 import { app, BrowserWindow } from 'electron';
-import { join } from 'path';
-import { pathToFileURL } from 'url';
 import { ConfigStore } from '@main/config/store';
 import { ConfigWatcher } from '@main/config/watch';
 import { Keys } from '@common/config/keys';
-import { Windows } from '@main/windows';
-import registerAll from '@main/ipc-invokes';
-// import { server } from './server';
+import { MainWindow } from '@main/windows';
+import registerIpc from '@main/ipc-invokes';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-const preload = join(__dirname, 'preload.js');
-let canShow: Promise<void>;
-
-async function createWindow() {
-  const loadUrl = isDevelopment
-    ? 'http://localhost:3000'
-    : pathToFileURL(join(__dirname, './renderer/index.html')).toString();
-  Windows.main = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload,
-      devTools: isDevelopment,
-    },
+function createWindow() {
+  return MainWindow.create({
+    width: 1600,
+    height: 900,
     show: false,
   });
-  canShow = new Promise<void>(res => {
-    Windows.main.once('ready-to-show', () => {
-      res();
-    });
-  });
-  await Windows.main.loadURL(loadUrl);
-
-  if (isDevelopment) {
-    Windows.main.webContents.toggleDevTools();
-  }
-}
-
-function registerIpc() {
-  registerAll();
 }
 
 async function loadConfig() {
@@ -49,17 +19,18 @@ async function loadConfig() {
   ConfigWatcher.Instance.addWatchHandler(Keys.clientConfiguration, async change => {
     if (change.eventType === 'change') {
       await store.readConfig();
-      store.sendToRender(Windows.main);
+      store.sendToRender(MainWindow.Instance);
     }
   });
 }
 
 app.whenReady().then(async () => {
-  const asyncWorks = Promise.all([createWindow(), loadConfig(), canShow]);
+  createWindow();
   registerIpc();
+  const asyncWorks = Promise.all([MainWindow.canShow, loadConfig()]);
   await asyncWorks;
-  Windows.main.show();
-  ConfigStore.Instance.sendToRender(Windows.main);
+  MainWindow.Instance.show();
+  ConfigStore.Instance.sendToRender(MainWindow.Instance);
 });
 
 app.on('window-all-closed', () => {
--- src/main/windows.ts
+++ src/main/windows.ts
@@ -1,7 +1,52 @@
-import type { BrowserWindow } from 'electron';
+import { BrowserWindow, BrowserWindowConstructorOptions } from 'electron';
+import { pathToFileURL } from 'url';
+import { join } from 'path';
 
-export const Windows: {
-  main: BrowserWindow
-} = {
-  main: null as unknown as BrowserWindow,
-};
+const isDevelopment = process.env.NODE_ENV === 'development';
+const preload = join(__dirname, 'preload.js');
+
+export class MainWindow extends BrowserWindow {
+  static canShow: Promise<void>;
+
+  static #instance: MainWindow;
+  static get Instance() {
+    return MainWindow.#instance;
+  }
+
+  static create(options: Omit<BrowserWindowConstructorOptions, 'webPreferences'>) {
+    if (!MainWindow.#instance) {
+      MainWindow.#instance = new MainWindow(options);
+    }
+    return MainWindow.#instance;
+  }
+
+  private constructor(options: Omit<BrowserWindowConstructorOptions, 'webPreferences'>) {
+    super({
+      ...options,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        preload,
+        devTools: isDevelopment,
+      },
+      show: false,
+    });
+    const loadUrl = isDevelopment
+      ? 'http://localhost:3000'
+      : pathToFileURL(join(__dirname, './renderer/index.html')).toString();
+
+    MainWindow.canShow = new Promise<void>(res => {
+      this.once('ready-to-show', () => {
+        res();
+      });
+    });
+
+    (async () => {
+      await this.loadURL(loadUrl);
+
+      if (isDevelopment) {
+        this.webContents.toggleDevTools();
+      }
+    })();
+  }
+}
